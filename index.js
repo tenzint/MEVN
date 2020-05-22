e8282cbb213419eb9bc1a6d225d4dc589fa87bbc
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-05-22 13:41:49 -0400

    created db related files. Temporarily stuck at mongod shutting down instantaneously

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
  
+const messages = require('.db/messages');
+
 const app = express();
  
 app.use(morgan('tiny'));
@@ -15,6 +17,12 @@ app.get('/', (req, res) => {
     });
 });
  
+app.get('/messages', (req, res) => {
+    messages.getAll().then((messages) => {
+        res.json(messages);
+    });
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`listening on ${port}`);
